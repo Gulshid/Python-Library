8233b99705bc07afbd333babde7984781a3dcc6d
Gulshid Zada <gulshidzada4@gmail.com>
2026-06-10 18:48:10 +0500

    Array Operation done

---
+++ numpy_library/array_operation.py
@@ -0,0 +1,40 @@
+import numpy as np
+#  Array Operation
+# a + b ==> addition
+# a - b ==> subtraction
+# a * b ==> Multiplication
+# np.sin(a) ==> sin
+# a ** 2 ==> square
+
+a = np.array([2,4,6])
+b = np.array([1,3,5])
+print("Array of 2D :")
+print(a)
+print(b)
+
+print("=========")
+#  Array Operation   a + b ==> addition
+print("addition Operation of 2D :")
+print("a + b :", a + b)
+
+
+print("=========")
+#  Array Operation   a - b ==> subtraction
+print("Subtraction Operation of 2D :")
+print("a - b :", a - b)
+
+print("=========")
+#  Array Operation   a * b ==> Multiplication
+print("Multiplication Operation of 2D :")
+print("a * b :", a * b)
+
+print("=========")
+#  Array Operation   np.sin(a) ==> sin
+print(" sin Operation of 2D :")
+print("sin(a) :", np.sin(b))
+
+print("=========")
+#  Array Operation   a ** 2 ==> square
+print(" Square Operation of 2D :")
+print("Square of a :", a ** 2)
+
