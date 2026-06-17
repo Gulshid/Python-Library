8f8cbe54e3447b2fefafe28bc324ffd32dd7d567
Gulshid Zada <gulshidzada4@gmail.com>
2026-06-17 16:07:24 +0500

    Scatter plot done

---
+++ matplotlib_library/scatter_plot.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+
+#  Scatter Plot
+x = [5, 7, 8, 9, 4]
+y = [10, 12, 15, 20, 5]
+
+plt.scatter(x, y)
+
+plt.title("*** Scatter Plot Example ***")
+
+plt.xlabel("X Values")
+
+plt.ylabel("Y Values")
+
+plt.show()
